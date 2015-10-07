7be3e7496b46f4f9a314f805a2d14b378385984b
mobapptuts <nige.henshaw@gmail.com>
2015-10-07 13:38:48 +0900

    Performance grid gallery - Slight mods in preparation for the performance profiling series

--- app/src/main/java/nigelhenshaw/com/cameraintenttutorial/GalleryApplication.java
+++ app/src/main/java/nigelhenshaw/com/cameraintenttutorial/GalleryApplication.java
@@ -12,6 +12,6 @@ public class GalleryApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Fresco.initialize(this);
+        // Fresco.initialize(this);
     }
 }
